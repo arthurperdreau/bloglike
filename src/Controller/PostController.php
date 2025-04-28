eda501d7450689da1b47620000bdec45e5fa668d
perdreauarthur <arthur.perdreau@outlook.fr>
2025-04-28 09:31:38 +0200

    add file

---
+++ src/Controller/PostController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Image;
+use App\Entity\Post;
+use App\Form\CommentType;
+use App\Form\ImageType;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/', name: 'posts')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/post/new', name: 'new_post', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $manager):Response
+    {
+        if(!$this->getUser() || !in_array("ROLE_ADMIN",$this->getUser()->getRoles())){
+            return $this->redirectToRoute('app_login');
+        }
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setAuthor($this->getUser());
+            $manager->persist($post);
+            $manager->flush();
+            return $this->redirectToRoute('posts');
+        }
+        return $this->render('post/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'show_post', priority: -1 )]
+    public function show(Post $post, Request $request, EntityManagerInterface $manager): Response
+    {
+        if (!$post){
+            return $this->redirectToRoute("posts");
+        }
+        if(!$this->getUser()){
+            $this->redirectToRoute("app_login");
+        }
+        $comment=new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPost($post);
+            $comment->setAuthor($this->getUser());
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('show_post', ['id' => $post->getId()]);
+        }
+
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/edit/{id}', name: 'edit_post' )]
+    public function edit(Post $post, Request $request, EntityManagerInterface $manager):Response
+    {
+        if (!$post){
+            return $this->redirectToRoute("posts");
+        }
+        if(in_array("ROLE_ADMIN",$this->getUser()->getRoles())){
+            $this->redirectToRoute('app_login');
+        }
+        if($this->getUser() !== $post->getAuthor()){
+            return $this->redirectToRoute("posts");
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($post);
+            $manager->flush();
+            return $this->redirectToRoute('posts');
+        }
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/post/delete/{id}', name: 'delete_post',  )]
+    public function delete(Post $post, EntityManagerInterface $manager):Response
+    {
+        if(in_array("ROLE_ADMIN",$this->getUser()->getRoles())){
+            $this->redirectToRoute('app_login');
+        }
+        if ($post){
+            $manager->remove($post);
+            $manager->flush();
+        }
+        return $this->redirectToRoute('posts');
+
+    }
+
+    #[Route('/post/addimage/{id}', name: 'post_image')]
+    public function addImage(Post $post, Request $request, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser() || !$post)
+        {
+            return $this->redirectToRoute('app_login');
+        }
+        if($post->getAuthor() !== $this->getUser())
+        {
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $image->setPost($post);
+            $manager->persist($image);
+            $manager->flush();
+            return $this->redirectToRoute('post_image', ['id' => $post->getId()]);
+        }
+
+
+        return $this->render('post/images.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/remove-image/{id}', name: 'remove_image')]
+    public function removeImage(Image $image, Request $request, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser() || !$image)
+        {
+            return $this->redirectToRoute('app_login');
+        }
+        if($image->getPost()->getAuthor() !== $this->getUser())
+        {
+            return $this->redirectToRoute('show_post', ['id' => $image->getPost()->getId()]);
+        }
+        $postId = $image->getPost()->getId();
+
+        $manager->remove($image);
+        $manager->flush();
+
+        return $this->redirectToRoute('post_image', ['id' => $postId]);
+    }
+
+
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[UniqueEntity(
+    fields: ['title'],
+    message: 'Ce titre existe déjà. Merci d\'en choisir un autre.'
+)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\Length(min:5, max: 90,)]
+    #[Assert\Regex(
+        pattern: '/^(?!.*\b(choucroute)\b).*/i',
+        message: 'Le mot "choucroute" est interdit.'
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[Assert\Length(min:15,)]
+    #[Assert\Regex(
+        pattern: '/^(?!.*\b(pastèque|champignon)\b).*/i',
+        message: 'Les mots "pastèque" et "champignon" sont interdits.'
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'post', orphanRemoval: true)]
+    private Collection $comments;
+
+    /**
+     * @var Collection<int, Image>
+     */
+    #[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'post')]
+    private Collection $images;
+
+    /**
+     * @var Collection<int, Like>
+     */
+    #[ORM\OneToMany(targetEntity: Like::class, mappedBy: 'post')]
+    private Collection $likes;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+        $this->images = new ArrayCollection();
+        $this->likes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): static
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): static
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getPost() === $this) {
+                $image->setPost(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Like>
+     */
+    public function getLikes(): Collection
+    {
+        return $this->likes;
+    }
+
+    public function addLike(Like $like): static
+    {
+        if (!$this->likes->contains($like)) {
+            $this->likes->add($like);
+            $like->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLike(Like $like): static
+    {
+        if ($this->likes->removeElement($like)) {
+            // set the owning side to null (unless already changed)
+            if ($like->getPost() === $this) {
+                $like->setPost(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isLikedBy(User $user): bool
+    {
+        foreach ($this->likes as $like) {
+            if ($like->getAuthor() === $user) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
