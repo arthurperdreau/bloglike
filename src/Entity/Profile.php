c7072b7517426c29ddb64a2f394b45e39075b3f6
perdreauarthur <arthur.perdreau@outlook.fr>
2025-04-30 16:24:19 +0200

    add file

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -13,8 +13,53 @@ class Profile
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\OneToOne(inversedBy: 'profile', cascade: ['persist', 'remove'])]
+    private ?Image $picture = null;
+
+    #[ORM\OneToOne(inversedBy: 'profile', cascade: ['persist', 'remove'])]
+    private ?User $ofUser = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $displayName = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPicture(): ?Image
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?Image $picture): static
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getOfUser(): ?User
+    {
+        return $this->ofUser;
+    }
+
+    public function setOfUser(?User $ofUser): static
+    {
+        $this->ofUser = $ofUser;
+
+        return $this;
+    }
+
+    public function getDisplayName(): ?string
+    {
+        return $this->displayName;
+    }
+
+    public function setDisplayName(?string $displayName): static
+    {
+        $this->displayName = $displayName;
+
+        return $this;
     }
 }
